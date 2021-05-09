d47e13e0e4c547f00c62756e17e40dc934bdb250
Kim Hammar <kimham@kth.se>
2021-05-09 11:38:27 +0200

    plot dynamics model

---
+++ examples/v1/plot_dynamics_model.py
@@ -0,0 +1,13 @@
+from gym_optimal_intrusion_response.util.plots import plot_dynamics_model
+
+def plot():
+    print("reading model")
+    path = "/home/kim/workspace/gym-optimal-intrusion-response/examples/v1/new_defender_dynamics_model.json"
+    defender_dynamics_model = plot_dynamics_model.read_model(path)
+    print("model read")
+    #plot_dynamics_model.plot_all(defender_dynamics_model)
+    plot_dynamics_model.plot_ids_infra_and_one_machine(defender_dynamics_model)
+    # plot_dynamics_model.plot_ids_dynamics_2(defender_dynamics_model)
+
+if __name__ == '__main__':
+    plot()
