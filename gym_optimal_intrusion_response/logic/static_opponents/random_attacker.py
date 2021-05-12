aeb8581f079d5583f33ceb092694340214be62b8
Kim Hammar <kimham@kth.se>
2021-05-12 11:18:40 +0200

    bugfix

--- gym_optimal_intrusion_response/logic/static_opponents/random_attacker.py
+++ gym_optimal_intrusion_response/logic/static_opponents/random_attacker.py
@@ -7,7 +7,7 @@ class RandomAttacker(StaticAttacker):
     def __init__(self, num_actions : int):
         super().__init__(num_actions=num_actions)
 
-    def action(self, env) -> int:
+    def action(self, env, t) -> int:
         legal_actions = list(filter(lambda x: env.is_attack_action_legal(x, env.env_config, env.env_state), self.actions))
         if len(legal_actions) == 0:
             print("no legal actions")
