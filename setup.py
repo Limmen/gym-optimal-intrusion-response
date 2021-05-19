7b6965c96b5e66c44e7ab472b014417b97db1eda
Kim Hammar <kimham@kth.se>
2021-05-19 11:20:21 +0200

    pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(name='gym_optimal_intrusion_response',
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Build Tools',
-          'License :: Creative Commons Attribution-ShareAlike 4.0 International',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6'
   ]
